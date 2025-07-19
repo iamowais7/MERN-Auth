62b51b680162383e10268faefa001658c92d0165
OWAIS KHAN <khan.owais0555@gmail.com>
2025-07-19 23:01:11 +0530

    Home created

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import {assets} from '../assets/assets.js'
-
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const navigate = useNavigate();
   return (
     <div className='w-full flex items-center justify-between p-4 sm:p-6 sm:px-24 absolute top-0'>
       <img src={assets.logo} className='w-28 sm:w-32'/>
-      <button className='flex items-center gap-2 border border-gray-500 rounded-full px-5 py-2 text-gray-800 hover:bg-gray-100 transition-all'>
+      <button onClick={() => navigate('/login')} 
+      className='flex items-center gap-2 border border-gray-500 rounded-full px-5 py-2 text-gray-800 hover:bg-gray-100 transition-all'>
         Login <img src={assets.arrow_icon} alt="" /></button>
     </div>
   )
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const Login = () => {
+
+  const [state,setState] = useState('Sign Up')
   return (
     <div>
-      <h1>Login Page</h1>
+      <img src={assets.logo} alt="Logo" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
+      <div>
+        <h2></h2>
+      </div>
     </div>
   )
 }
